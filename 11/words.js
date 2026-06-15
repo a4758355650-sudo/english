d68139d671fbcd9382a7f9573e7c9305efbdfd24
王廷哲 <a4758355650@gmail.com>
2026-06-15 15:40:17 +0800

    first commit

---
+++ 11/words.js
@@ -0,0 +1,341 @@
+const wordData = `
+schedule|安排行程
+confirm|確認
+available|可用的
+client|客戶
+delay|延遲
+shipment|出貨
+purchase|購買
+budget|預算
+contract|合約
+attend|參加
+invoice|發票
+refund|退款
+employee|員工
+manager|經理
+department|部門
+appointment|預約
+customer|顧客
+equipment|裝置
+salary|薪水
+repair|修理
+interview|面試
+approve|核准
+cancel|取消
+estimate|估計
+guarantee|保證
+recommend|推薦
+update|更新
+maintain|維持
+solution|解決方案
+temporary|暫時的
+urgent|緊急的
+reschedule|重新安排
+deadline|截止日期
+meeting|會議
+advertisement|廣告
+application|申請
+reservation|預定
+payment|付款
+receipt|收據
+project|專案
+reserve|預定
+replace|更換
+respond|回復
+survey|問卷/調查
+transfer|調職/轉移
+authorize|授權
+brochure|小冊子
+commute|通勤
+destination|目的地
+enroll|註冊/報名
+facility|設施
+fare|車資
+inventory|庫存
+luggage|行李
+merchandise|商品
+renovate|翻新
+shortage|短缺
+venue|活動地點
+draw|提領
+workshop|研討會
+administration|行政管理
+appoint|指派任命
+appreciation|感謝
+assignment|任務
+attachment|附件
+authority|權限
+badge|識別證
+briefing|簡報
+colleague|同事
+commitment|責任
+correspondence|通信
+document|公文
+enlarge|放大
+executive|經理主管人員
+extension|分機號碼
+facilitate|使容易/使順暢
+figure out|解決
+floor|發言權
+forward|轉寄
+go ahead|開始
+implement|實施
+in case|以防萬一
+interrupt|打斷
+label|標籤
+mailbox|郵件信箱
+mention|提到
+minutes|會議記錄
+motion|臨時動議
+on duty|值班
+on the same page|達成共識
+partition|隔板
+photocopier|影印機
+postpone|延後
+process|過程
+projector|投影機
+pros and cons|利弊得失
+punch in|上班打卡
+put through|轉接電話
+receptionist|接待人員
+recipient|收件者
+reduction|減少
+reform|改革
+registered mail|掛號信
+relevant|有關的
+rely|依賴
+requisition form|需求單
+reset|重新設定
+resolution|決議
+return address|寄件人地址
+shareholder|股東
+staple|釘書機
+stationery|文具
+stay on top of|獲知最新消息
+stockroom|儲藏室
+subject|主旨
+subordinate|下屬
+summarize|摘要
+take part|參加
+version|版本
+reality|現實
+maturity|成熟
+popularity|人氣度
+loyalty|忠實
+specialty|專長
+address|地址
+adjourn|中止
+afford|負擔得起
+agenda|議程
+agreement|一致
+arrangement|安排
+assurance|保證
+attract|吸引
+avoid|避免
+bargain|議價/便宜貨
+bid|投標
+boardroom|會議室
+brainstorming|腦力激盪
+cancellation|取消
+clause|條款
+commercial|商業的/廣告
+competition|競爭
+compromise|妥協
+conclude|得出結論
+confidential|秘密
+convince|說服
+demonstrate|示範
+designate|標示
+detach|分離
+determine|決定
+develop|發展
+direct marketing|直效行銷
+distribution|分配/配銷/流通
+downsize|縮編
+enclose|封入附上
+enterprise|企業/公司
+establish|創立
+evaluate|評估
+franchise|經銷權
+go over|查看/重溫
+headquarters|總部
+inevitable|不可避免
+legitimate|合法的
+mission|任務
+negotiate|協商
+objection|反對
+offer|報價
+open to|開放的空間
+party|當事人
+persuasion|說服
+presentation|簡報
+productive|具建設性
+progress|進展
+register|登錄
+reputation|聲譽
+retail|零售
+satisfy|履行契約
+session|會議
+strategy|策略
+terminate|使結束
+variety|變化
+action|行為
+addition|增加
+collection|收集
+conclusion|結論
+observation|觀察
+access card|門禁卡
+annual leave|年假
+apply|申請
+background|經歷
+basis|基準
+candidate|申請人
+career|職業
+compensate|補償
+conduct|行為
+contract employee|約聘人員
+employ|聘用
+evaluation|評估
+fill in|職務代理人
+follow up|持續追蹤
+full time|全職
+headhunter|獵人頭公司
+hiring criteria|錄取標準
+human resources|人力資源
+incentive|刺激
+intern|實習生
+interviewee|面試者
+job description|工作說明
+job fair|徵才博覽會
+job opening|工作職缺
+lay off|解雇
+leave without pay|留職停薪
+move up|提升
+multitasking|多重任務處理
+on track|有望成功/步入正軌
+on the job training|在職訓練
+overtime|加班
+pay check|薪資支票/工資
+pension|退休金
+personnel|人員/人事部
+pink slip|解雇通知單
+position|職位
+probationary period|試用期
+proficient|精通的
+profile|個人簡介
+promote|升遷
+prospective|潛在的
+qualification|資格
+recognition|公開資格
+reject|拒絕
+relocate|調職
+report to|直屬
+resignation|辭職
+resume|履歷表
+retire|退休
+seniority|資歷
+severance pay|資遣費
+specialize|專精
+take over|接管
+track record|績效紀錄
+turnover|員工流動率
+unemployment rate|失業率
+working hours|工作時間
+motivation|動機
+emotional|情緒的
+remove|移除
+mobile|可移動的
+assemble|配裝
+assemble line|生產線
+automated|自動化
+batch|一批
+circumstance|情況
+collaboration|合作
+component|零件
+concentrate|集中注意力
+condition|狀況
+conform|符合
+customize|訂做
+defect|缺陷
+device|設備
+dimension|尺寸
+disruption|干擾
+durability|耐久性
+electronics|電子產品
+enhance|提高
+examine|仔細檢查
+experiment|實驗
+fluctuate|波動
+gadget|小機件
+glitch|小故障
+high tech industry|高科技產業
+in house|內部完成
+inspect|檢查
+install|安裝
+launch|產品上市
+life test|測試使用壽命
+machinery|機械裝置
+maintenance|維護/保養
+manufacturer|製造業者
+mass production|大量生產
+mechanic|技師
+on site|現場的
+output|產量
+outsource|外包
+overproduction|產量過剩
+packaging|包裝
+patent|專利
+plant|工廠
+power failure|停電
+procedure|程序
+product line|產品線
+production|生產
+production capacity|產能
+quality control|品質管理
+raw material|原料
+recall|回收
+rival|競爭者
+shut down|停工
+strike|罷工
+subcontract|分包
+supervisor|監督者
+warehouse|倉庫
+wholesale|批發
+marriage|婚姻
+storage|儲藏
+coverage|涵蓋
+orphanage|孤兒院
+village|村莊
+abundant|大量的
+adjustment|調整
+appliance|器具
+automatically|自動地
+buyers guide|買家指南
+carriage free|免付運費
+carrier|運輸公司
+carry|有出售
+cash on delivery|貨到付現
+catalog|目錄
+chain store|連鎖店
+`;
+
+function uniqueWords(data){
+    let seen = new Set();
+
+    return data.trim().split("\n").map(line => {
+        let [english, meaning] = line.split("|");
+        return [english.trim(), meaning.trim()];
+    }).filter(word => {
+        if(seen.has(word[0])){
+            return false;
+        }
+
+        seen.add(word[0]);
+        return true;
+    });
+}
+
+window.wordData = wordData;
+window.getVocabularyWords = function(){
+    return uniqueWords(wordData);
+};
